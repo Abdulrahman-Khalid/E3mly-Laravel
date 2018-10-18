f941ced6e52a15cdfd9630358a56d7936122e011
Abdulrahman El Shafei <abdulrahman.khalid@bk.ru>
2018-10-18 15:44:22 +0200

    delete a paragraph

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <div class="jumbotron text-center">
         <h1>Welcome To <span class="text-primary">E3mly</span></h1>
-        <p>This is a laravel application from the "Laravel From Scratch" YouTube series</p>
         <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
     </div>
 @endsection
